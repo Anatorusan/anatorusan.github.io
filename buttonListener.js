66675455ddce6146b8627c02dc5a7a911b02c3b7
Anatolii <anatorusan@gmail.com>
2023-04-21 21:32:29 +0300

    deleted previous app

--- buttonListener.js
+++
@@ -1,9 +0,0 @@
-const swBtn = document.getElementById('swichButtn');
-
-export const buttonListener = (...events) => {
-    events.forEach((event) => {
-        swBtn.addEventListener('click', event);
-        console.log('buttonListener is active')
-    })
-    
-}
--- storage.js
+++
@@ -1,8 +0,0 @@
-export const saveData = (key, value) => {
-    localStorage.setItem(key, value);
-}
-
-export const loadData = (key) => {
-    const loadedData = localStorage.getItem(key);
-    return loadedData ? loadedData : false;
-}
